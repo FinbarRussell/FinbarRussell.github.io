66baf5d0f11c879660f94f5415a5342d0ca0ee07
Finbar <finbar_r@hotmail.co.uk>
2024-09-03 20:26:11 +0100

    formatting changes

--- website.js
+++ website.js
@@ -1,23 +1,24 @@
   
 window.onload = function() {
   openMenu()
-
 }
 
-  function openMenu(){
-    document.getElementById('menu-bar').addEventListener('click', function() {
+function openMenu() {
+  document.getElementById('menu-bar').addEventListener('click', function(event) {
       const navLinks = document.getElementById('nav-links');
       navLinks.classList.toggle('show');
+      
+      // Prevent the click event from propagating to the document
+      event.stopPropagation();
   });
-  }
 
-/*
-  // Close the dropdown if clicked outside
-  window.onclick = function(event) {
-    if (!event.target.matches('.menu-toggle') && !event.target.matches('.menu-toggle *')) {
-      const dropdown = document.getElementById('dropdown');
-      if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
+  // Close the navbar when clicking anywhere outside
+  document.addEventListener('click', function(event) {
+      const navLinks = document.getElementById('nav-links');
+      const menuBar = document.getElementById('menu-bar');
+      
+      if (!navLinks.contains(event.target) && !menuBar.contains(event.target)) {
+          navLinks.classList.remove('show');
       }
-    }
-  }*/
+  });
+}
